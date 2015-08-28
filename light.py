ccd5adf83147ed0cb98a775dbaa7d20ecd205f04
Brycen <dorsay@live.ca>
2015-08-28 00:18:59 -0700

    Busy, success, unstable, and failure endpoints

--- light.py
+++ light.py
@@ -1,18 +1,28 @@
-__author__ = 'Brycen'
-
 from blink1.blink1 import blink1
 import time
 
-# b1 = Blink1()
+
+def busy():
+    activate(232, 66, 244)
 
 
-def blink():
+def success():
+    activate(22, 226, 172)
+
+
+def failure():
+    activate(232, 34, 0)
+
+
+def unstable():
+    activate(232, 146, 0)
+
+
+def activate(r, g, b):
+    print("activate")
     with blink1() as b1:
-        print("blink!")
-        for i in range(5):
-            b1.fade_to_rgb(10, 255, 255, 255)
-            time.sleep(.1)
-            b1.fade_to_rgb(10, 0, 0, 0)
-            time.sleep(.1)
-
-
+        for i in range(10):
+            b1.fade_to_color(100, (r, g, b))
+            time.sleep(.8)
+            b1.fade_to_color(100, (0, 0, 0))
+            time.sleep(.8)
--- routes.py
+++ routes.py
@@ -1,1 +1,37 @@
-__author__ = 'Brycen'
+from flask import Blueprint
+from flask import request
+import light
+
+api = Blueprint('buildAPI', __name__)
+
+
+@api.route("/busy", methods=['POST'])
+def busy():
+    light.busy()
+    return "busy"
+
+
+@api.route("/success", methods=['POST'])
+def success():
+    light.success()
+    return "success"
+
+
+@api.route("/failure", methods=['POST'])
+def failure():
+    light.failure()
+    return "failure"
+
+
+@api.route("/unstable", methods=['POST'])
+def unstable():
+    light.unstable()
+    return "unstable"
+
+
+# Just to test request arguments
+@api.route("/words")
+def respond():
+    return request.args.get("words")
+
+
